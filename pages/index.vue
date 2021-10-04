6713934aa286bb694c73409bd7941efcfe34e3bf
Viktor Krejcir <viktor.krejcir@fibiworks.com>
2021-10-04 17:18:10 +0200

    header added

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
 <template>
+ <div>
+   <b-container>
+     <b-jumbotron class="head" header="Chcu parkovat v Brně!" lead="Aktuální přehled obsazenosti parkovacích domů v Brně">
+     
+     </b-jumbotron>
+    <!-- <b-col cols="12" class="head">
+      <image src="header.jpg" width="100%" height="100%"/>
+    </b-col> -->
+   </b-container>
    <ParkHouses :parkingData="parkingData"/>
+ </div>
 </template>
 
+<style>
+.head{
+    background: linear-gradient(to right, #575464ba, #908e92cc), url(~static/jam.jpg) no-repeat top center;
+  color: white;
+  /* background-image: url("~static/header.jpg"); */
+  /* background-size: 100%; */
+}
+</style>
 <script>
 import parkHouses from "@/components/ParkHouses.vue";
 
