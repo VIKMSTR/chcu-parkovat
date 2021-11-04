0df0c1b68d27d20dca020e972a3782d3971d62c5
Viktor Krejcir <viktor.krejcir@fibiworks.com>
2021-11-04 17:49:29 +0100

    errorpage added

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+    <b-container class="align-middle lead">
+    <h1 v-if="error.statusCode === 404">Ajaj, kde nic, tu nic.</h1>
+    <h1 v-else>Něco se stalo, co se stát nemělo.</h1>
+    <b-alert show variant="danger" class="text-left">
+      <p>Tady jsi zaparkoval špatně - parkovací místo {{error.statusCode}}.</p>
+    </b-alert>
+    <NuxtLink to="/">Zpátky na stránku, kde něco je.</NuxtLink>
+   </b-container>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'error' // you can set a custom layout for the error page
+  }
+</script>
