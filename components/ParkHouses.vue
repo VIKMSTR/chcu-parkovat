e88e14ce90e0ce3df2327d6bbf01f2148ac0a5a4
Viktor Krejcir <viktor.krejcir@fibiworks.com>
2021-11-05 17:35:29 +0100

    trying vertical group

--- components/ParkHouses.vue
+++ components/ParkHouses.vue
@@ -8,16 +8,16 @@
             <b-td variant="success" class="text-center align-middle lead numcell" show v-if="parking.attributes.capacity_procent > 25" >{{parking.attributes.free}}</b-td>
             <b-td variant="warning" class="text-center align-middle lead numcell" show v-if="parking.attributes.capacity_procent <= 25" >{{parking.attributes.free}}</b-td>
             <b-td variant="success" class="text-right align-middle" show v-if="parking.attributes.capacity_procent > 25"  > 
-              <b-btn-group>
+              <div class="btn-group-vertical">
               <b-button variant="primary" v-bind:href="'http://www.google.com/maps/place/' +parking.attributes.Latitude +',' + parking.attributes.Longitude">mapa</b-button>
               <b-button variant="info" v-bind:href="'http://www.waze.com/ul?ll=' +parking.attributes.Latitude +',' + parking.attributes.Longitude + '&z1240'">Waze</b-button>
-              </b-btn-group>
+              </div>
             </b-td>
             <b-td variant="warning" class="text-right align-middle" show v-if="parking.attributes.capacity_procent <= 25" > 
-              <b-btn-group>
+              <div class="btn-group-vertical">
                 <b-button variant="primary" v-bind:href="'http://www.google.com/maps/place/' +parking.attributes.Latitude +',' + parking.attributes.Longitude">mapa</b-button>
                 <b-button variant="info" v-bind:href="'http://www.waze.com/ul?ll=' +parking.attributes.Latitude +',' + parking.attributes.Longitude + '&z=1240'">Waze</b-button>
-              </b-btn-group>
+              </div>
             </b-td>
          </b-tr>
        </b-table-simple> 
